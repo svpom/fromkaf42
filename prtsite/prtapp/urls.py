a527fb25fddabea35c05f0e5a15104a758fd626a
svpom <pominovsergeyvict@gmail.com>
2017-03-18 23:56:42 +0300

    Have made really normal main page. Added summary_about_trees_root.html and made it work properly. But only with not real info in it

--- prtsite/prtapp/urls.py
+++ prtsite/prtapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
+    url(r'^summary-about-trees-root/$', views.summary_about_trees_root,
+        name='summary_about_trees_root'),
 ]
--- prtsite/prtapp/views.py
+++ prtsite/prtapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
     return render(request, 'prtapp/main_page.html', {})
+
+
+def summary_about_trees_root(request):
+    return render(request, 'prtapp/summary_about_trees_root.html', {})
